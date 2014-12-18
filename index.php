2de5147742c3cf92379f7ec84554ed667da45cb9
ConanXie <xjj416@gmail.com>
2014-12-18 20:20:36 +0800

    主页优化
    主页优化

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     var list = [
         <?php
             while ($reco_row = mysql_fetch_array($reco_result)) {
-                echo "{content: '<div class=list id=list1><a href=><img src=".$reco_row["image"]."><p class=reco-name>".$reco_row["name"]."</p><p class=reco-price>".$reco_row["price"]."<span class=yuan>元</span></p></a>'},";
+                echo "{content: '<div class=list id=list1><a href=detail.php?id=".$reco_row["id"]."><img src=".$reco_row["image"]."><p class=reco-name>".$reco_row["name"]."</p><p class=reco-price>".$reco_row["price"]."<span class=yuan>元</span></p></a>'},";
             }
         ?>
     ];
@@ -40,7 +40,7 @@
             $so_sql = "SELECT * FROM dish WHERE spec = 1 LIMIT 3;";
             $so_result = mysql_query($so_sql);
             while ($so_row = mysql_fetch_array($so_result)) {
-                echo '<div class="so-dish"><a href=""><img src="'.$so_row["image"].'" alt=""><p>'.$so_row["name"].'</p><p><span class="so-price">'.$so_row["price"].'<span class="yuan">元</span> <span class="ori-price">'.$so_row["oriprice"].'元</span></p></a></div>';
+                echo '<div class="so-dish"><a href="detail.php?id='.$so_row["id"].'"><img src="'.$so_row["image"].'" alt=""><p>'.$so_row["name"].'</p><p><span class="so-price">'.$so_row["price"].'<span class="yuan">元</span> <span class="ori-price">'.$so_row["oriprice"].'元</span></p></a></div>';
             }
         ?>
     </div>
@@ -62,7 +62,7 @@
         $all_sql = "SELECT * FROM dish ORDER BY id DESC LIMIT 1;";
         $all_result = mysql_query($all_sql);
         $all_row = mysql_fetch_array($all_result);
-        echo '<a href=""><dl><dt style="background-image: url('.$all_row["image"].');"></dt><dd><h3>'.$all_row["name"].'</h3><p class="all-skecth">'.$all_row["sketch"].'</p><p class="all-price">'.$all_row["price"].'<span class="yuan">元</p><p class="sell-num">已售'.$all_row["sellnum"].'份</p></dd></dl></a>';
+        echo '<a href="detail.php?id='.$all_row["id"].'"><dl class="dish-dl"><dt class="dish-dt" style="background-image: url('.$all_row["image"].');"></dt><dd class="dish-dd"><h3>'.$all_row["name"].'</h3><p class="all-skecth">'.$all_row["sketch"].'</p><p class="all-price">'.$all_row["price"].'<span class="yuan">元</p><p class="sell-num">已售'.$all_row["sellnum"].'份</p></dd></dl></a>';
     ?>
     </div>
 </section>
