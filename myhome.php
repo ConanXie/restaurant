9022ede75eaadecec545a8db0157810fdb5ec894
ConanXie <xjj416@gmail.com>
2014-12-18 13:08:04 +0800

    个人主页设计

--- myhome.php
+++ myhome.php
@@ -5,6 +5,38 @@
         header("Location: ".$config_basedir);
         exit();
     }
-    echo '<p>欢迎你，'.$_SESSION['USERNAME'].'</p>';
 ?>
-<a href="logout.php">退出</a>
+<link rel="stylesheet" href="css/home.css">
+<div id="head">
+    <div id="master">&#xe606;</div>
+    <p class="master-name"><?php echo $_SESSION['USERNAME']; ?></p>
+</div>
+<div class="manager">
+    <div class="e-mana">
+        <a href="">&#xe675;</a>
+        <p>购物车</p>
+    </div>
+    <div class="e-mana">
+        <a href="">&#xe8bc;</a>
+        <p>已完成订单</p>
+    </div>
+    <div class="e-mana">
+        <a href="">&#xe8bb;</a>
+        <p>待评价订单</p>
+    </div>
+    <div class="e-mana">
+        <a href="">&#xe6f2;</a>
+        <p>联系方式</p>
+    </div>
+    <div class="e-mana">
+        <a href="">&#xeafd;</a>
+        <p>意见反馈</p>
+    </div>
+    <div class="e-mana">
+        <a href="logout.php">&#xe626;</a>
+        <p>退出</p>
+    </div>
+</div>
+<?php
+    require('footer.php');
+?>
