b663f18c6db150fbd7b92c95cf50a76d9117c649
ConanXie <xjj416@gmail.com>
2014-12-22 15:12:39 +0800

    订单页面
    订单结构，订单样式，表单处理

--- order.php
+++ order.php
@@ -5,20 +5,25 @@
         exit();
     } else {
         $_SESSION['confirm'] = 'confirm';
-        echo '<form action="confirm.php" method="post">';
-        echo '<input type="radio" name="choice" value="1" checked>外卖';
-        echo '<div>';
+        echo '<link rel="stylesheet" href="css/order.css">';
+        echo '<form action="confirm.php" method="post" name="order">';
+        echo '<h3>消费方式</h3>';
+        echo '<input type="radio" name="choice" class="choice" value="0" checked> 店内消费<br />';
+        echo '<input type="radio" name="choice" class="choice" value="1"> 外卖';
+        echo '<div id="contact-box">';
         $contact_result = mysql_query("SELECT * FROM contact WHERE userid = ".$_SESSION['USERID'].";");
         while ($contact_row = mysql_fetch_array($contact_result)) {
-            echo '<input type="radio" name="contact" value="'.$contact_row['id'].'">联系方式';
+            echo '<div class="contact">';
+            echo '<input type="radio" name="contact" value="'.$contact_row['id'].'" checked> 联系方式';
             echo '<p>联系人: '.$contact_row['name'].'</p>';
             echo '<p>联系电话: '.$contact_row['phone'].'</p>';
             echo '<p>外卖地址: '.$contact_row['address'].'</p>';
+            echo '</div>';
         }
         echo '</div>';
-        echo '<input type="radio" name="choice" value="0">店内消费';
+        echo '<h3 class="order-list">订单列表</h3>';
         echo "<table border=1>";
-        echo "<tr><td>菜名</td><td>单价</td><td>数量</td></tr>";
+        echo "<tr><td>菜名</td><td>单价（元）</td><td>数量</td></tr>";
         $cost = 0;
         foreach($_POST['checkdish'] as $mycheckbox) {
             $dish_info = explode(' ', $mycheckbox);
@@ -39,8 +44,36 @@
             echo '<input type="checkbox" name="checkdish[]" value="'.$mycheckbox.' '.$dish_row['price'].'" checked style="display: none;">';
         }
         echo '<input type="checkbox" name="cost" checked value="'.$cost.'" style="display: none;">';
-        echo '<p>总费用：'.$cost.'</p>';
+        echo '<p class="sum-cost">总费用：'.$cost.'<span class="yuan">元</span></p>';
         echo '<input type="submit" value="提交订单">';
         echo "</form>";
-    }
+    }?>
+    <script type="text/javascript">
+        var order = {
+            choiceIn: document.order.choice[0],
+            choiceOut: document.order.choice[1],
+            contactBox: document.querySelector('#contact-box'),
+            contactWay: document.querySelectorAll('.contact'),
+            init: function () {
+                this.checkContact();
+            },
+            checkContact: function () {
+                var that = this;
+                this.choiceOut.addEventListener('click', function () {
+                    if (!that.contactWay.length) {
+                        alert('无联系方式，请在联系方式里添加');
+                        that.choiceIn.checked = true;
+                    } else {
+                        that.contactBox.style.display = 'block';
+                    }
+                }, false);
+                this.choiceIn.addEventListener('click', function () {
+                    that.contactBox.style.display = 'none';
+                }, false);
+            }
+        };
+        order.init();
+    </script>
+    <?php
+    require('footer.php');
 ?>
