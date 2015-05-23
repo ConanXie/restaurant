1b223e173d23ba29066c0751731b654a22faa671
ConanXie <xjj416@gmail.com>
2015-05-23 22:00:39 +0800

    数据库连接
    数据库操作方法修改

--- config.php
+++ config.php
@@ -1,7 +1,12 @@
 <?php
-    $conn = mysql_connect("localhost", "root", "conan") or die("数据库连接失败".mysql_error());
+    /*$conn = mysql_connect("localhost", "root", "conan") or die("数据库连接失败".mysql_error());
     mysql_select_db("restaurant", $conn);
-    mysql_query("set names utf8");
-    // $config_basedir = "http://192.168.1.112:8080/restaurant/";
+    mysql_query("set names utf8");*/
+    $mysqli = new mysqli('localhost', 'root', 'conan', 'restaurant');
+    $mysqli->query('set names utf8');
+    if (mysqli_connect_errno()) {
+        echo mysqli_connect_error();
+    }
+    // $config_basedir = "http://192.168.1.100:8080/restaurant/";
     $config_basedir = "http://localhost:8080/restaurant/";
 ?>
